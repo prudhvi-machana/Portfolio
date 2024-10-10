7e83f1118a23a1c9f80124e034ee4c0b8f70d706
prudhvi-machana <prudhvimachana24@gmail.com>
2024-10-11 00:14:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+document.querySelectorAll('.skill-card, .project-card').forEach(card => {
+    observer.observe(card);
+});
